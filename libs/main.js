73297637e9850bdb501e0451f08394ad7efdd071
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-22 20:23:20 -0600

    Update main.js

--- libs/main.js
+++ libs/main.js
@@ -1,4 +1,4 @@
-var width,height,frameCount=0;
+var width,height,frameCount=0,fillOn=true,strokeOn=true;;
 function size(w, h){
   width=w;
   height=h;
@@ -16,12 +16,15 @@ function fullScreen(){
   c.style.width=w;
   c.style.height=h;
 }
-
+//document.getElementById("pCanvas").getContext("2d").
 function line(x1,y1,x2,y2){
-  document.getElementById("pCanvas").getContext("2d").line(x1,y1,x2,y2);
+  if(strokeOn){
+    document.getElementById("pCanvas").getContext("2d").line(x1,y1,x2,y2);
+    document.getElementById("pCanvas").getContext("2d").stroke();
+  }
 }
-
 function stroke(a,b,c,d){
+  strokeOn=true;
   if(a&&b&&c&&d){
     document.getElementById("pCanvas").getContext("2d").strokeStyle="rgba("+a+","+b+","+c+","+d+")";
   }else if(a&&b&&c&&!d){
@@ -33,6 +36,7 @@ function stroke(a,b,c,d){
   }
 }
 function fill(a,b,c,d){
+  fillOn=true;
   if(a&&b&&c&&d){
     document.getElementById("pCanvas").getContext("2d").fillStyle="rgba("+a+","+b+","+c+","+d+")";
   }else if(a&&b&&c&&!d){
@@ -41,5 +45,24 @@ function fill(a,b,c,d){
     document.getElementById("pCanvas").getContext("2d").fillStyle="rgba("+a+","+a+","+a+","+b+")";
   }else{
     document.getElementById("pCanvas").getContext("2d").fillStyle="rgb("+a+","+a+","+a+")";
+  }
+}
+
+function strokeWidth(a){
+  ctx.lineWidth=a+"";
+}
+function noStroke(){
+  strokeOn=false;
+}
+function noFill(){
+  fillOn=false;
+}
+function rect(x,y,w,h){
+  if(fillOn){
+    document.getElementById("pCanvas").getContext("2d").fillRect(x,y,w,h);
+  }
+  if(strokeOn){
+    document.getElementById("pCanvas").getContext("2d").rect(x,y,w,h);
+    document.getElementById("pCanvas").getContext("2d").stroke();
   }
 }
